6f890896d15596c38e0caa3f4822144d05a5b61a
cjwalle97 <cjwalle97@gmail.com>
2017-01-19 16:37:07 -0600

    Initial Commit
    Initial Commit

---
+++ TrafficLightFSM/LightFSM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace TrafficLightFSM
+{
+    class LightFSM
+    {
+        private int m_timer;
+        private bool m_active;
+
+        public LightFSM()
+        {
+
+        }
+    }
+}
